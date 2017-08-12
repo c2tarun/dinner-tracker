d67a9314a1d633ea34b030e5c2fe5a16b7bf8c2c
Tarun K. Mall <c2tarun@hotmail.com>
2017-08-12 13:02:49 -0700

    Cleaning up some hardcoded values.

--- index.js
+++ index.js
@@ -1,37 +1,78 @@
 var dashButton = require('node-dash-button');
+var moment = require('moment-timezone');
+var readConfig = require('read-config');
 var AWS = require('aws-sdk');
-var moment = require('moment');
 
-var awsCredentials = new AWS.SharedIniFileCredentials({profile:'dinner-tracker'});
+var homeDirectory = process.env['HOME'];
+var config = readConfig(homeDirectory + '/.config/dinner-tracker/config');
+
+var awsCredentials = new AWS.SharedIniFileCredentials({
+    profile: 'dinner-tracker',
+    filename: config.awsCredentials
+});
+
 AWS.config.update({
-	region: 'us-west-2',
-	endpoint: 'https://dynamodb.us-west-2.amazonaws.com'
+    region: 'us-west-2'
 });
 AWS.config.credentials = awsCredentials;
 
-var dinnerDash = dashButton("fc:a6:67:85:4f:5a", null, null, 'all');
+var dynamodb = new AWS.DynamoDB();
+dynamodb.setEndpoint('https://dynamodb.us-west-2.amazonaws.com');
+
+var sns = new AWS.SNS();
+sns.setEndpoint('https://sns.us-west-2.amazonaws.com');
+
+var dinnerDash = dashButton(config.dinnerDashSSID, null, null, 'all');
 dinnerDash.on("detected", () => {
-	console.log("Yay dinner is ready!!");
-	var dinnerTime = moment().format();
-	console.log("dinner done at " + dinnerTime);
-
-	var params = {
-		Item: {
-			"dinnerTime": {
-				S: dinnerTime
-			}
-		},
-		TableName: "dinner-timings"
-	};
-
-	var dynamodb = new AWS.DynamoDB();
-	console.log(params);
-	dynamodb.putItem(params, function(err, data) {
-		if (err) {
-			console.log(err, err.stack);
-		} else {
-			console.log(data);
-		}
-	});
+    makeEntryInDynamoDB((err) => {
+        console.error("Sending data to dynamo failed ", err);
+    }, (dinnerTime) => {
+        console.log("Data sent to Dynamo");
+        sendSMS(dinnerTime);
+    });
 });
 
+var makeEntryInDynamoDB = function(error, success) {
+    var seattleTime = moment.tz('America/Los_Angeles');
+    console.log("Dinner Time " + seattleTime.format().toString());
+    var params = {
+        Item: {
+            "dinnerTime": {
+                S: seattleTime.format().toString()
+            }
+        },
+        TableName: config.dinnerTableName
+    };
+
+    console.log(params);
+    dynamodb.putItem(params, function(err, data) {
+        if (err) {
+            error(err);
+        } else {
+            success(seattleTime);
+        }
+    });
+}
+
+var sendSMS = function(dinnerTime) {
+    var day = dinnerTime.format('YYYY-MMMM-DD');
+    var time = dinnerTime.format('h:mm:ss');
+    var smsMessage = "Dinner time on " + day + " is " + time;
+
+    console.log("SMS: " + smsMessage);
+
+    var snsParams = {
+        Message: smsMessage,
+        TopicArn: config.dinnerSnsArn
+    }
+
+    console.log("SNS Arn: " + config.dinnerSnsArn);
+
+    sns.publish(snsParams, function(err, data) {
+        if (err) {
+            console.error("Unable to send message. Error JSON: ", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Results from sending message: ", JSON.stringify(data, null, 2));
+        }
+    });
+}
--- test.js
+++
@@ -1,9 +0,0 @@
-var moment = require('moment');
-
-makeDynamoDBEntry = function() {
-	console.log("called makeDynamoDBEntry");
-	console.log(moment().format());
-	console.log(moment().utc().format());
-}
-
-makeDynamoDBEntry();
